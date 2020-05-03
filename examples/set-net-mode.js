e9fcabea318cd32cdfb5510e5a1e4ca5f3bd2beb
atrukhny <atrukhny@bp-3.com>
2020-05-03 12:45:36 +0300

    Improve error messages

--- examples/set-net-mode.js
+++ examples/set-net-mode.js
@@ -11,12 +11,12 @@ const LTE_BAND = '7FFFFFFFFFFFFFFF'; // All supported
 router.getToken(function(error, token) {
   router.login(token, USERNAME, PASSWORD, function (error) {
     if (error) {
-      console.error('Failed to login', error);
+      console.error('Failed to login:', error);
       return;
     }
     router.setNetworkMode(token, NETWORK_MODE, NETWORK_BAND, LTE_BAND, function (error, response) {
       if (error) {
-        console.error('Failed to set network mode');
+        console.error('Failed to set network mode:', error);
       } else {
         console.log('Set network mode result:', response);
       }
